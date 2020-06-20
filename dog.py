456db64bab1f932202bedcbfbdf652f89eeddb2a
jozefkamensky <8290e9YM>
2020-06-20 10:44:16 +0200

    remove extended computation from dog filter

--- dog.py
+++ dog.py
@@ -2,23 +2,20 @@ from skimage.filters import gaussian
 from skimage.io import imsave
 from skimage import util
 import skimage.data as d
-import math
 
 k = 1.6
-epsilon = 0.3
-p = 18
-fi = 0.6
+epsilon = 0.05
 
 image = d.camera()
 sigmas = [1, 2, 3, 4, 5, 6]
 for sigma in sigmas:
     low_sigma = sigma
     high_sigma = k * sigma
-    im = (1 + p) * gaussian(image, low_sigma) - p * gaussian(image, high_sigma)
+    im = gaussian(image, low_sigma) - gaussian(image, high_sigma)
     for i in range(0, im.shape[0]):
         for j in range(0, im.shape[1]):
             if im[i][j] >= epsilon:
-                im[i][j] = 1
+                im[i][j] = 0
             else:
-                im[i][j] = 1 + math.tanh(fi * (im[i][j] - epsilon))
+                im[i][j] = 1
     imsave('out/dog_' + str(sigma) + '.png', util.img_as_ubyte(im))
